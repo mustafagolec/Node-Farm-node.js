bffaf293dd939aebc9bc08e9fa4609e917f1620f
Mustafa Göleç <mustafagolec36@gmail.com>
2024-11-05 17:35:29 +0300

    Main Page
    Products now appear on main page.
    Using data.json file to store information about products. And calling them to their placeholder places in html files. Rendering each product on template-card.html with their information.

--- index.js
+++ index.js
@@ -31,23 +31,46 @@ console.log('Will read file!'); */
 
 ///////////////////////////
 //SERVER
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+
+    if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    return output;
+}
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8'); //We have seperated api code here to prevent load api data every time when user gets to /api route 
 const dataObj = JSON.parse(data); //This code will be executed once when the site loads for the first time.
 
-const server = http.createServer((req, res) => {
+const server = http.createServer((req, res) => {  //this section is called each time there is a request
     const pathName = req.url;
 
+    //Overview Page
     if (pathName === '/' || pathName === '/overview') {
-        res.end('This is the Overview');
-    }
+        res.writeHead(200, { 'content-type': 'text/html' });
+
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+
+        res.end(output);
+    }//Product Page
     else if (pathName === '/product') {
         res.end('This is the Product');
-    }
+    }//API
     else if (pathName === '/api') {
         res.writeHead(200, { 'content-type': 'application/json' });
         res.end(data);
-    }
+    }//Not found
     else {
         res.writeHead(404, {
             'Content-type': 'text/html',
